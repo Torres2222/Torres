49c6340c6745ea2729aaa4bcdd72251dbc24aa05
Torres2222 <2350474c@umich.mx>
2024-09-20 18:36:17 -0600

    Add files via upload

---
+++ ejemplotareaformulas.R
@@ -0,0 +1,14 @@
+---
+title: "pagina de prueba para la tarea"
+author: "Angel Torres Renero "
+editor: visual 
+---
+  
+  #Ejemplo de uno de mis formulas: 
+En este ejemplo carga mis formulas de interes simple desde mi repitorio de [github]()
+
+#se cargan las funciones de manera remota: 
+
+https://github.com/Torres2222/Torres/upload/main
+
+source(https://github.com/Torres2222/Torres/edit/main/README.simple.R)
